a3d806e351310f1538789d182fa1f0801d6db249
= <hopekr@dongguk.edu>
2020-05-18 18:29:58 +0900

    setup files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Switch, Route} from 'react-router-dom'
+import './App.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
+import Default from './component/Default';
+import Main from './component/Main';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>  
+      <Header/>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route component={Default}/>
+        </Switch>
+      <Footer/>
+    </React.Fragment>
   );
 }
 
---
+++ src/component/Default.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Default(props) {
+    return (
+        <div>
+            404 Page
+        </div>
+    )
+}
+
+Default.propTypes = {
+
+}
+export default Default
